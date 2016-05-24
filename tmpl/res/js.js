347c5dd778c118e1698d30ae7e83b3bd41f60141
Elliot <key.draw@gmail.com>
2016-05-24 05:15:30 +0000

    first commit

---
+++ tmpl/res/js.js
@@ -0,0 +1,171 @@
+var dropzone;
+var target;
+var uploadInput;
+
+function startup() {
+    context_element = document.getElementById("context-menu");
+    dropzone  = document.getElementById('dropzone');
+    uploadInput = document.getElementById("uploadfile");
+    
+    //context menu stuff
+    document.addEventListener('contextmenu', function(e) {
+        if (e.target.className.split(" ")[0] == "file") {
+            target = e.target;
+            filename = target.parentNode.textContent+target.getAttribute("filetype")
+            context_element.innerHTML = "<a>⇣ Download</a>" +
+                "<a>📂 Preview / Edit</a>" +
+                "<div></div>" +
+                "<a onclick=deleteFile('"+filename+"');>✖ Delete</a>" +
+                "<a>✎ Rename</a>"+
+                "<div></div>" +
+                "<a>� Info</a>" +
+                "<a>🔗 Get Link</a>";
+            var view_bottom = window.innerHeight + window.pageYOffset; //bottom of viewport
+            if (view_bottom - e.pageY > 300) {
+                context_element.style.top = e.pageY+"px";
+                context_element.style.left = e.pageX+"px";
+            } else {
+                context_element.style.top = (e.pageY - 300)+"px";
+                context_element.style.left = e.pageX+"px";
+            }
+            context_element.style.display = "block";
+            
+        } else if (e.target.id == "dropzone") {
+             context_element.innerHTML = "\
+                <a onclick='selectFile();'>⇡ Upload</a>\
+                <a>✚ New Text File</a>";
+            var view_bottom = window.innerHeight + window.pageYOffset;
+            if (view_bottom - e.pageY < 100) {
+                context_element.style.top = (e.pageY-100)+"px";
+                context_element.style.left = e.pageX+"px";
+            } else {
+                context_element.style.top = e.pageY+"px";
+                context_element.style.left = e.pageX+"px";
+            }
+            context_element.style.display = "block";
+        }else {
+            context_element.style.display = "none";
+        }
+        e.preventDefault();
+    }, false);
+    
+    document.addEventListener('click', function(e) {
+        context_element.style.display = "none";
+    });
+    
+    //file upload stuff
+	dropzone.addEventListener('dragover', cancel, false);
+	dropzone.addEventListener('dragenter', cancel, false);
+	dropzone.addEventListener('drop', function(e) { 
+		e = e || window.event;
+		if (e.preventDefault) { e.preventDefault(); }
+
+		var fs = e.dataTransfer.files;
+		uploadFiles(fs)
+		return false;
+	});
+	uploadInput.addEventListener('change', function(e) {
+		uploadFiles(uploadInput.files);
+		uploadInput.value = null;
+	});
+}
+
+Element.prototype.remove = function() {
+    this.parentElement.removeChild(this);
+}
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+    for(var i = this.length - 1; i >= 0; i--) {
+        if(this[i] && this[i].parentElement) {
+            this[i].parentElement.removeChild(this[i]);
+        }
+    }
+}
+
+function deleteFileElement(eldel) {
+    eldel.parentNode.style.webkitAnimationDirection = "reverse";
+    eldel.parentNode.style.webkitAnimationName = "expand";
+    
+    fun = function (elem) {
+        return function(e) {
+            elem.parentNode.remove();
+            elem.remove();
+        };
+    }(eldel);
+    eldel.parentNode.addEventListener("animationend", fun);
+}
+
+function addFileElement(name, ext) {
+    dropzone.insertAdjacentHTML('afterbegin', "<a href='/upl/"+ name + "." +ext +"'><div class='file "+ ext +"' filetype='."+ext+"'></div>"+name+"</a>");
+    dropzone.firstChild.style.webkitAnimationName = "expand";
+
+    fun = function (elem) {
+        return function(e) {
+            elem.style.webkitAnimationName = "";
+        };
+    }(dropzone.firstChild);
+    dropzone.firstChild.addEventListener("animationend", fun);
+}
+
+function selectFile() {
+    uploadInput.click();
+}
+
+//file upload stuff
+
+function cancel(e) {
+	if (e.preventDefault) { 
+		e.preventDefault(); 
+	}
+	return false;
+}
+
+function uploadFiles(fs) {
+	if (fs.length == 0) {
+		console.log('no file to upload!');
+		return;
+	}
+	var fd = new FormData();
+	var xhr = new XMLHttpRequest();
+	    xhr.open('POST', '/', true);
+	
+    fun = function(name, ext) {
+        return function() {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 0) {
+                        console.log('A state error occurred!');
+                    } else if (xhr.status == 200) {
+                        console.log('file uploaded!');
+                        addFileElement(name, ext);
+                    }
+                }
+            };
+    }(fs[0].name.substr(0, fs[0].name.lastIndexOf('.')),fs[0].name.substr(fs[0].name.lastIndexOf('.') + 1));
+	xhr.onreadystatechange = fun;
+	    
+	var f = fs[0];
+	fd.append('uploadfile', f, f.name);
+
+	xhr.send(fd);
+}
+
+function deleteFile(name) {
+	var xhr = new XMLHttpRequest();
+	    xhr.open('DELETE', "/upl/" + name, true);
+    //apply to target
+    fun = function(eldel) {
+        return function() {
+                console.log(xhr.readyState);
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 0) {
+                        console.log('A state error occurred!');
+                    } else if (xhr.status == 200) {
+                        console.log('file uploaded!');
+                        deleteFileElement(eldel);
+                    }
+                }
+            };
+    }(target);
+    
+    xhr.onreadystatechange = fun;
+    xhr.send();
+}
---
+++ wiki.go
@@ -0,0 +1,180 @@
+package main
+
+import(
+	"io/ioutil"
+	"io"
+	"net/http"
+	"html/template"
+	"regexp"
+	"fmt"
+	"os"
+	"time"
+	"strings"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+type UpFile struct {
+	Name string
+	Type string
+	Size int64
+	Time time.Time
+}
+var files = make(map[string]UpFile)
+
+var templates = template.Must(template.ParseFiles("tmpl/edit.html", "tmpl/view.html", "tmpl/upload.html"))
+var validPath = regexp.MustCompile("^/(edit|view)/([a-zA-Z0-9]+)$")
+
+func (f UpFile) Path() string {
+	return "upl/"+f.Name+f.Type
+}
+
+func (f UpFile) TypeNoDot() string {
+	return f.Type[1:]
+}
+
+func Ext(path string) string {
+	for i := len(path) - 1; i >= 0 && !os.IsPathSeparator(path[i]); i-- {
+		if path[i] == '.' {
+			return path[i:]
+		}
+	}
+	return ""
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	err := templates.ExecuteTemplate(w, tmpl+".html" ,p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func makeHandler(fn func (http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w,r)
+			return
+		}
+		fn(w, r, m[2])
+	}
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+	fmt.Println("edit")
+	if r.Method == "GET" {
+		p, err := loadPage(title)
+		if err != nil {
+			p = &Page{Title: title}
+		}
+		renderTemplate(w, "edit", p)
+	} else if r.Method == "POST" {
+		fmt.Println("save")
+		body := r.FormValue("body")
+		p := &Page{Title: title, Body: []byte(body)}
+		err := p.save()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		http.Redirect(w, r, "/view/" + title, http.StatusFound)
+	}
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	fmt.Println("view")
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/" + title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+
+func RegisterFile(f os.FileInfo) error {
+	
+	suffix := Ext(f.Name())
+	name := strings.TrimSuffix(f.Name(), suffix)
+	size := f.Size()
+	
+	files[f.Name()] = UpFile{Name: name, Type: suffix, Size: size, Time: time.Now()}
+	
+	fmt.Println(files[f.Name()].Path())
+	return nil
+}
+
+func DirectoryToUpFiles() {
+	files, _ := ioutil.ReadDir("./upl/")
+	for _, fi := range files {
+		RegisterFile(fi)
+	}
+}
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		fmt.Println("default GET")
+		templates.ExecuteTemplate(w, "upload.html" ,files)
+		//deal with err from this
+	} else if r.Method == "POST" {
+		fmt.Println("default POST")
+		r.ParseMultipartForm(32 << 20)
+		file, handler, err := r.FormFile("uploadfile")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+		f, err := os.Create("./upl/"+handler.Filename)
+		if err != nil {
+		    fmt.Println(err)
+		    return
+		}
+		defer f.Close()
+		io.Copy(f, file)
+		fi, _ := f.Stat()
+		RegisterFile(fi)
+		
+	}
+}
+
+func staticHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method =="GET" {
+		http.ServeFile(w, r, r.URL.Path[1:])
+	} else if r.Method == "DELETE" {
+		fmt.Println("remove: " + r.URL.Path[1:])
+		err := os.Remove(r.URL.Path[1:])
+		if (err != nil) {
+			fmt.Println(err)
+		}
+		
+		delete(files, r.URL.Path[len("/upl/"):])
+		//handle errors?
+	}
+}
+
+func main() {
+	DirectoryToUpFiles()
+	http.HandleFunc("/", defaultHandler)
+	http.HandleFunc("/tmpl/res/", staticHandler)
+	http.HandleFunc("/upl/", staticHandler)
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.ListenAndServe(":8080", nil)
+}
